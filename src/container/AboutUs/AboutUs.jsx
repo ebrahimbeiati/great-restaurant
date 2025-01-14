39cff6dbf0198d6f664677edb8b17d329466611d
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2025-01-14 20:04:06 +0000

    About us

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 
+import { images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className="app__aboutus app__bg flex__center section__padding" id="about">
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G_overlay" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Our restaurant is a place where you can enjoy the best of both worlds. We offer a wide range of dishes from all over the world, and our chefs are always experimenting with new flavors and ingredients.
+        </p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Our restaurant has been serving delicious food to the people of this city for over 20 years. We have a long history of providing great service and amazing food to our customers, and we are always looking for ways to improve.
+        </p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+    </div>
   </div>
 );
 
