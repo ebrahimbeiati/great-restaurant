f2797ed85e795d2826d102a431bc1c36d2623f66
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2025-01-14 17:46:30 +0000

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import images from '../../constants/images';
 
 import './Navbar.css';
 
-const Navbar = () => (
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  return (
   <nav className='app__navbar'>
     <div className='app__navbar-logo'>
       <img src={images.logo} alt='logo' />
@@ -21,7 +23,24 @@ const Navbar = () => (
       <div/>
       <a href='/' className='p__opensans'>Book a Table</a>
     </div>
+    <div className='app__navbar-smallscreen'>
+      <GiHamburgerMenu fontSize={30} color='white' onClick={()=>{setToggle(true)}} />
+        {toggle &&(
+             <div className='app__navbar-smallscreen-overlay flex__center slide-bottom'>
+            <MdOutlineRestaurantMenu fontSize={30} color='white' className='overlay__close' onClick={()=>{setToggle(false)}} />
+          <ul className='app__navbar-smallscreen-links'>
+            <li><a href='#home'>Home</a></li>
+            <li><a href='#about'>About</a></li>
+            <li><a href='#menu'>Menu</a></li>
+            <li><a href='#contact'>Contact</a></li>
+          </ul>
+
+        </div>
+        )}
+     
+    </div>
   </nav>
-);
+  )
+}
 
 export default Navbar;
